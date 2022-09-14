ffcf71b9d5b0d2d12ef772dee97bac0510e4f0d7
Andrew De Rango <93727693+andrewderango@users.noreply.github.com>
2022-09-14 19:14:52 -0400

    Add files via upload

---
+++ PP ATOI Projections/Age Adjusted/Defence/previous_seasons_regr_d.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn import linear_model
+import time
+
+start_time = time.time()
+
+big_board = pd.read_csv('/users/andrewderango/Documents/Python/NHL Projections/PP ATOI Projections/Age Adjusted/Defence/PP ATOI Projector - Age-Adjusted Defence Board.csv')
+column_names = big_board.columns.values.tolist()
+# print(big_board)
+streaks = pd.DataFrame(columns=['Player','Range','Year I','Year II','Year III','Year IV'])
+for index in range(len(big_board.index)):
+    player_timeline = []
+
+    for column in big_board:
+        player_timeline.append(big_board[column][index])
+    # print(player_timeline)
+    
+    for item in range(len(player_timeline)):
+        try:
+            if player_timeline[item] == float(player_timeline[item]) and player_timeline[item+1] == float(player_timeline[item+1]) and player_timeline[item+2] == float(player_timeline[item+2]) and player_timeline[item+3] == float(player_timeline[item+3]):
+                # print(player_timeline[item], player_timeline[item+1], player_timeline[item+2], player_timeline[item+3])
+
+                streak_dict = {'Player': player_timeline[0], 'Range': column_names[item][0:4] + column_names[item+3][4:7], 'Year I': player_timeline[item], 'Year II': player_timeline[item+1], 'Year III': player_timeline[item+2], 'Year IV': player_timeline[item+3]}
+                streaks = streaks.append(streak_dict, ignore_index = True)
+        except:
+            pass
+
+    # print()
+
+# print(streaks)
+
+X = streaks[['Year I', 'Year II', 'Year III']]
+y = streaks['Year IV']
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+print()
+print('--DEFENCE 3-YEAR REGRESSION--')
+print('Year I Coefficient: ' + str(regr.coef_[0]))
+print('Year II Coefficient: ' + str(regr.coef_[1]))
+print('Year III Coefficient: ' + str(regr.coef_[2]))
+print('Results are adjusted for age variance')
+
+end_time = time.time()
+elapsed_time = end_time-start_time
+print('\nLinear regression of ' + str(len(streaks.index)) + ' instances performed in ' + str(round(elapsed_time,2)) + ' seconds.')
---
+++ PP Goals60 Projections/Forwards/previous_seasons_regr_f.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from sklearn import linear_model
+import time
+
+start_time = time.time()
+
+#Must adjust after duplication!
+models_tested_qty = 3
+
+big_board = pd.read_csv('/users/andrewderango/Documents/Python/NHL Projections/PP Goals60 Projections/Forwards/2021-22 PP Goals60 Projector - Forward Board.csv')
+column_names = big_board.columns.values.tolist()
+possible_models = ['Model I', 'Model II', 'Model III', 'Model IV', 'Model V', 'Model VI', 'Model VII', 'Model VIII', 'Model IX', 'Model X']
+models_tested = possible_models[0:models_tested_qty]
+
+X = big_board[models_tested]
+y = big_board['2021-22 Actual Score']
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+coefficient_sum = 0
+print()
+print('--FORWARDS 3-YEAR REGRESSION--')
+for model in range(len(X.columns)):
+    print(possible_models[model] + ' Coefficient: ' + str(regr.coef_[model]))
+    coefficient_sum += regr.coef_[model]
+print('\nSum of coefficients: ' + str(round(coefficient_sum,2)))
+
+positive_coefficient_models = []
+for model in range(len(X.columns)):
+    if regr.coef_[model] > 0:
+        positive_coefficient_models.append(possible_models[model])
+    else:
+        print('Removing ' + possible_models[model] + ' from regression analysis due to poor partial correlation...')
+
+X = big_board[positive_coefficient_models]
+y = big_board['2021-22 Actual Score']
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+coefficient_sum = 0
+positive_coefficient_model_counter = 0
+print('\n--SIMPSON RE-EVALTION--')
+for model in range(len(models_tested)):
+    if models_tested[model] in X.columns:
+        print(models_tested[model] + ' Coefficient: ' + str(regr.coef_[positive_coefficient_model_counter]))
+        coefficient_sum += regr.coef_[positive_coefficient_model_counter]
+        positive_coefficient_model_counter += 1
+    else:
+        print(models_tested[model] + ' Coefficient: 0')
+
+print('\nSum of coefficients: ' + str(coefficient_sum))
+
+end_time = time.time()
+elapsed_time = end_time-start_time
+print('\nMultiple linear regression of ' + str(len(big_board)) + ' instances performed in ' + str(round(elapsed_time,2)) + ' seconds.')
