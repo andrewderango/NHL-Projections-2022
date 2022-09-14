d97c368dd62de65cde44528e71988ac408eb8b38
Andrew De Rango <93727693+andrewderango@users.noreply.github.com>
2022-09-14 19:12:57 -0400

    Add files via upload

---
+++ EV Shots60 Projections/Forwards/previous_seasons_regr_f.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn import linear_model
+import time
+
+start_time = time.time()
+
+big_board = pd.read_csv('/users/andrewderango/Documents/Python/NHL Projections/EV Shots60 Projections/Forwards/2021-22 EV Shots60 Projector - Forward Board.csv')
+column_names = big_board.columns.values.tolist()
+print(big_board)
+
+X = big_board[['Model I', 'Model II']]
+y = big_board['2021-22 Actual Score']
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+print()
+print('--FORWARDS 3-YEAR REGRESSION--')
+print('Model I Coefficient: ' + str(regr.coef_[0]))
+print('Model II Coefficient: ' + str(regr.coef_[1]))
+print('Results are adjusted for age variance')
+
+end_time = time.time()
+elapsed_time = end_time-start_time
+print('\nLinear regression of ' + str(len(big_board.columns)) + ' instances performed in ' + str(round(elapsed_time,2)) + ' seconds.')
---
+++ EV Shots60 Projections/Total/manual_regression.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df = pd.read_csv('/users/andrewderango/Documents/Python/NHL Projections/EV Goals60 Projections/Total/2021-22 EV Goals60 Projector - Filtered Board.csv')
